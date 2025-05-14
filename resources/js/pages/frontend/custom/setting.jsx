59f6bdfd9d989590e7f7ff2f5d1e9436b823f997
MD. Faysal Rahman <faysal35-3119@diu.edu.bd>
2025-05-15 00:09:58 +0600

    init

--- resources/js/pages/frontend/custom/setting.jsx
+++ resources/js/pages/frontend/custom/setting.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import EmployeeDashboardLayout from '../../../layouts/employeeDashboardLayout/EmployeeDashboardLayout';
-import AccountSettings from './AccountSettings';
 
 const setting = () => {
     return (
         <>
-        <EmployeeDashboardLayout>
-            <AccountSettings/>
-        </EmployeeDashboardLayout>
+            <EmployeeDashboardLayout>
+                <AccountSettings />
+            </EmployeeDashboardLayout>
         </>
     );
 };
--- resources/js/pages/frontend/home.jsx
+++ resources/js/pages/frontend/home.jsx
@@ -4,14 +4,13 @@ import MainLayout from '@/Layouts/MainLayout';
 import Hero from '@/components/frontend/home/hero.jsx';
 import HowItWorks from '@/components/frontend/home/HowItWorks';
 import CTA from '@/components/frontend/home/Cta';
-import MicroJobTalent from '../../components/frontend/home/MicroJobTalent';
 import ProComponent from '../../components/frontend/home/ProComponent';
 
 export default function Home({ categories, users, microTasks }) {
     return (
         <MainLayout>
             <Head title="Home" />
-            <Hero  />
+            <Hero />
             <HowItWorks />
             <div className="bg-gray-50 pt-5">
                 <MicroJobTalent />
