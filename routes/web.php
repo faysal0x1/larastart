48532a3ef421359876157ea4436985b01498eb6a
faysal0x1 <faysal35-3119@diu.edu.bd>
2025-09-01 22:38:01 +0600

    admin

--- routes/web.php
+++ routes/web.php
@@ -48,5 +48,4 @@ Route::middleware(['auth', 'verified'])->group(function () {
 require __DIR__ . '/settings.php';
 require __DIR__ . '/auth.php';
 require __DIR__ . '/admin.php';
-require __DIR__ . '/jayead.php';
 require __DIR__ . '/faysal.php';
