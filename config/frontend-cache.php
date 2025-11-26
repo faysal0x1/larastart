be55e0324c408587614dc2baa17da96b5ea7ef9b
faysal0x1 <faysal35-3119@diu.edu.bd>
2025-11-26 09:20:32 +0600

    ok

--- config/frontend-cache.php
+++ config/frontend-cache.php
@@ -1,3 +1,14 @@
 <?php
 
-return require app_path('Modules/ServiceWorker/Config/frontend-cache.php');
+// Build path relative to config directory
+$moduleConfigPath = dirname(__DIR__) . '/app/Modules/ServiceWorker/Config/frontend-cache.php';
+
+if (file_exists($moduleConfigPath)) {
+    return require $moduleConfigPath;
+}
+
+// Default configuration if module doesn't exist
+return [
+    'enabled'     => env('FRONTEND_CATEGORY_CACHE_ENABLED', false),
+    'ttl_minutes' => env('FRONTEND_CATEGORY_CACHE_TTL', 5),
+];
--- config/service-worker.php
+++ config/service-worker.php
@@ -1,3 +1,18 @@
 <?php
 
-return require app_path('Modules/ServiceWorker/Config/service-worker.php');
+// Build path relative to config directory
+$moduleConfigPath = dirname(__DIR__) . '/app/Modules/ServiceWorker/Config/service-worker.php';
+
+if (file_exists($moduleConfigPath)) {
+    return require $moduleConfigPath;
+}
+
+// Default configuration if module doesn't exist
+return [
+    'enabled'        => env('SERVICE_WORKER_ENABLED', false),
+    'cache'          => env('SERVICE_WORKER_CACHE', 'off'),
+    'cache_images'   => env('SERVICE_WORKER_CACHE_IMAGES', true),
+    'cache_api'      => env('SERVICE_WORKER_CACHE_API', false),
+    'max_cache_size' => env('SERVICE_WORKER_MAX_CACHE_SIZE', 100),
+    'cache_version'  => env('SERVICE_WORKER_CACHE_VERSION', '1'),
+];
